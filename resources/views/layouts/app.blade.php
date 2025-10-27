03ed771512750ed341a0fbf0e90ce877b501470d
Vithiaz <vithiaz@gmail.com>
2025-10-27 07:05:30 +0700

    feat: set header icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>@yield('title', config('app.name'))</title>
+  <link rel="icon" href="/logo.png" type="image/png">
   @vite(['resources/css/app.css','resources/js/app.js'])
   @livewireStyles
 </head>
